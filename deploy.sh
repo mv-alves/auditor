ea7f4540bea364442bc2f435823351a7d9de0453
Marcos Alves <marcos.vyny@gmail.com>
2016-05-23 11:13:32 -0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-cp /Users/marcos/NetBeansProjects/dist/auditor-compare.jar .;
+cp /Users/marcos/Documents/ilegra/sts/sts-pipeline-auditor/tools-helpers/auditor-review/build/libs/auditor-review-1.0.jar .;
 git commit -a -m "deploy";
 git push ;
 
